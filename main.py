113312ad71780049ebae2cde1ab16f6a09c281c2
Ben Lee <benzuvsteel@gmail.com>
2017-11-25 04:34:48 -0800

    completed steps 1-4.

--- main.py
+++ main.py
@@ -1,39 +1,56 @@
 import random
 
-allCodes = []
-answer = []
-keyPegs = []
+def main():
+    allCodes = []
+    answer = []
+    keyPegs = []
+    numTurns = 0
 
-# set initial guess to 1122
-guess = [1, 1, 2, 2]
+    # create the entire set of 1296 possible codes.
+    for peg1 in range(1, 7):
+        for peg2 in range(1, 7):
+            for peg3 in range(1, 7):
+                for peg4 in range(1, 7):
+                    allCodes.append([peg1, peg2, peg3, peg4])
 
-# create the entire set of 1296 possible codes.
-for peg1 in range(1, 7):
-    for peg2 in range(1, 7):
-        for peg3 in range(1, 7):
-            for peg4 in range(1, 7):
-                allCodes.append([peg1, peg2, peg3, peg4])
+    # set initial guess to 1122
+    guess = [1, 1, 2, 2]
 
-# randomly generate the correct answer.
-for pos in range(4):
-    answer.append(random.randint(1, 6))
+    # randomly generate the correct answer.
+    for pos in range(4):
+        answer.append(random.randint(1, 6))
 
-print(answer)
+    # play the guess.
+    playGuess(guess, answer, keyPegs, numTurns)
+    checkWin(keyPegs)
 
-
+def playGuess(gue, ans, key, turns):
+    setKeyPegs(gue, ans, key)
+    turns += 1
 
 def setKeyPegs(guess, answer, keys):
-    correctPositions = [False, False, False, False]
-    whiteKeyPegs = 0
+    nums = []
+    
+    for peg in answer:
+        nums.append(peg)
 
     for guessPos, guessNum in enumerate(guess):
         for answerPos, answerNum in enumerate(answer):
-            if (guessNum == answerNum):
-                if (guessPos == answerPos):
-                    correctPositions[guessPos] = True
+            if guessNum in nums:
+                if guessPos == answerPos:
+                    keys.append('b')
+                keys.append('w')
+                nums.remove(guessNum)
+                break
 
-    for guess in correctPositions:
-        if guess:
-            keys.append('b')
-            whiteKeyPegs -= 1
-           
+def checkWin(key):
+    if key == ['b', 'b', 'b', 'b']:
+        print("Guesser wins!")
+        return True
+    else:
+        print("Try again")
+        return False
+
+
+if __name__ == '__main__':
+    main()
